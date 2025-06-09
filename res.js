1662e48c514f22b7e8032c5ea2e251b10e08fd0b
codeAyush05 <ayushh.dev05@gmail.com>
2025-06-09 09:01:02 +0530

    'updated'

--- res.js
+++ res.js
@@ -1,19 +1,21 @@
-let a = document.querySelectorAll("P")
-for(let i=0 ; i< a.length ; i++){
-    a[i].addEventListener("mousemove" , change);
-}
+// /** @format */
 
-function change(){
-    this.style.backgroundColor = "BLUE"
-    this.innerHTML = "KHANA KHA KE JAIYOOO....."
-};
+// let a = document.querySelectorAll("P,h1");
+// for (let i = 0; i < a.length; i++) {
+//   a[i].addEventListener("mousemove", change);
+// }
 
- let b = document.querySelectorAll("h1")
-for(let j=0 ; j< b.length ; j++){
-    b[j].addEventListener("mousemove" , change2);
-}
+// function change() {
+//   this.style.backgroundColor = "BLUE";
+//   this.innerHTML = "KHANA KHA KE JAIYOOO.....";
+// }
 
-function change2(){
-    this.style.backgroundColor = "BLUE"
-    this.innerHTML = "KHANA KHA KE JAIYOOO....."
-};
+//  let b = document.querySelectorAll("h1")
+// for(let j=0 ; j< b.length ; j++){
+//     b[j].addEventListener("mousemove" , change2);
+// }
+
+// function change2(){
+//     this.style.backgroundColor = "BLUE"
+//     this.innerHTML = "KHANA KHA KE JAIYOOO....."
+// };
